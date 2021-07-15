56224e9376e0f519f798cbbb002da5057415d985
Melford-D <melfordbirakor@gmail.com>
2021-07-15 01:29:31 +0100

    updated the github icon on hex.html

--- hex.js
+++ hex.js
@@ -1,4 +1,5 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 
@@ -7,7 +8,7 @@ btn.addEventListener("click", function () {
   for (let i = 0; i < 6; i++) {
     hexColor += hex[getRandomNumber()];
   }
-  // console.log(hexColor);
+  console.log(hexColor);
 
   color.textContent = hexColor;
   document.body.style.backgroundColor = hexColor;
